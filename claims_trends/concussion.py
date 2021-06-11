19f5ebf8b771864cf27ff4300c05ba21d61a993c
ReturnoftheMak <50878639+ReturnoftheMak@users.noreply.github.com>
2021-06-11 12:36:03 +0100

    update for latest scm

--- claims_trends/concussion.py
+++ claims_trends/concussion.py
@@ -14,24 +14,32 @@ key_words = ['concussion', 'cranial', 'brain',
              'dizziness', 'fatigue', 'insomnia']
 
 
-# %%
+# %% Loading
 
 df_scm = pd.read_csv(r'C:\Users\makhan.gill\SQL_DATA\LMM_SCM_all.csv',
                      usecols=['ClaimDetailID', 'LossDescription', 'LossLocation'])
 df_cgen = pd.read_csv(r'C:\Users\makhan.gill\SQL_DATA\claim_data_general_all.csv',
-                      usecols=['ClaimDetailID', 'ClaimAdvisedDate', 'HandlingClass'])
+                      usecols=['ClaimDetailID', 'ClaimAdvisedDate', 'HandlingClass', 'UnderwritingYear'])
+
+df_scm.drop_duplicates(subset=['ClaimDetailID'], keep='last', inplace=True)
+
 df = df_scm.merge(df_cgen, how="left", on='ClaimDetailID')
 
-df.columns = ['claim_id', 'documents', 'loss_location', 'class', 'date']
-df = df[['date', 'loss_location', 'claim_id', 'class', 'documents']]
+df.columns = ['claim_id', 'documents', 'loss_location', 'underwriting_year', 'class', 'date']
+df = df[['date', 'underwriting_year', 'loss_location', 'claim_id', 'class', 'documents']]
 df["date"] = pd.to_datetime(df["date"], infer_datetime_format=True)
 
 stop_words_combined = get_stopwords()
 vectorizer = scikit_vectorizer(stop_words_combined, PorterTokenizer, CountVectorizer)
 
+
 # %% Liability only?
 
-df_liab = df[df['class'].isin(['Non Marine Liability', 'Marine Energy Liability', 'iBott General Liability'])]
+df_liab = df[df['class'].isin(['Non Marine Liability',
+                               'Marine Energy Liability',
+                               'iBott General Liability',
+                               'Specialty Disruptiton',
+                               'Accident & Health'])]
 
 # Chunk it?
 
@@ -39,7 +47,8 @@ grouped_list = []
 
 n = 10000
 
-list_df = [df[i:i+n] for i in range(0,len(df),n)]
+list_df = [df_liab[i:i+n] for i in range(0,len(df_liab),n)]
+
 
 # %% Chunking
 
@@ -58,16 +67,30 @@ relevant_claims = concussion_terms[concussion_terms.mentions > 0]
 
 df_relevant_claims = df[df.claim_id.isin(relevant_claims.claim_id.unique())]
 
+
 # %%
 
 df_scm_f = pd.read_csv(r'C:\Users\makhan.gill\SQL_DATA\LMM_SCM_all.csv')
 df_cgen_f = pd.read_csv(r'C:\Users\makhan.gill\SQL_DATA\claim_data_general_all.csv')
-df_scm_f = df_scm_f[df_scm_f['ClaimDetailID'].isin(relevant_claims.claim_id.unique())]
+
+df_scm_f = df_scm_f.sort_values(['ClaimDetailID', 'ReceivedDate'], axis=0).groupby('ClaimDetailID').tail(1)
+
+# Get a flag to say whether the claim is relevant
+
+df_scm_f['relevant_claim'] = np.where(df_scm_f['ClaimDetailID'].isin(relevant_claims.claim_id.unique()), "Relevant", "Irrelevant")
 
 df_export = df_scm_f.merge(df_cgen_f, how="left", on='ClaimDetailID')
+
+df_export = df_export[df_export['HandlingClass'].isin(['Non Marine Liability',
+                                                       'Marine Energy Liability',
+                                                       'iBott General Liability',
+                                                       'Specialty Disruptiton',
+                                                       'Accident & Health'])]
 
 df_export.to_csv(r'../data/pbi_data.csv')
 relevant_claims.to_csv(r'../data/relevant_claims.csv')
 
+df_claims = df_export[df_export['relevant_claim'] == 'Relevant']
+df_claims.to_csv(r'../data/flagged_claims.csv')
 
 # %%
